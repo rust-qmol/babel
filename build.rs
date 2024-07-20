3782d785e020164e20894c7376baba8e338322e1
chinder753 <chinder@foxmail.com>
2024-07-20 15:03:29 +0800

    changes

--- build.rs
+++ build.rs
@@ -2,36 +2,35 @@
 // use std::process::Command;
 
 fn main() {
-//     Command::new("git")
-//             .args(&["submodule", "init"])
-//                     .status()
-//                             .expect("git submodule init failed");
-//     Command::new("git")
-//                 .args(&["submodule", "update"])
-//                         .status()
-//                                 .expect("git submodule update faild");
+    //     Command::new("git")
+    //             .args(&["submodule", "init"])
+    //                     .status()
+    //                             .expect("git submodule init failed");
+    //     Command::new("git")
+    //                 .args(&["submodule", "update"])
+    //                         .status()
+    //                                 .expect("git submodule update faild");
 
-//     let dst = Config::new("openbabel").build();
+    //     let dst = Config::new("openbabel").build();
 
-//     println!("{}", dst.display());
+    //     println!("{}", dst.display());
 
-//     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-//     println!("cargo:rustc-link-lib=dylib=openbabel");
+    //     println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    //     println!("cargo:rustc-link-lib=dylib=openbabel");
 
-//     let bindings = bindgen::Builder::default()
-//         .clang_arg("-xc++")
-//         .clang_args(&[format!("-I{}/include/openbabel3", dst.display())])
-//         .header(format!("{}/include/openbabel3/openbabel/mol.h", dst.display()))
-//         .wrap_unsafe_ops(true)
-//         .generate_comments(true)
-//         .layout_tests(false)
-//         .generate()
-//         .expect("Unable to generate bindings");
+    //     let bindings = bindgen::Builder::default()
+    //         .clang_arg("-xc++")
+    //         .clang_args(&[format!("-I{}/include/openbabel3", dst.display())])
+    //         .header(format!("{}/include/openbabel3/openbabel/mol.h", dst.display()))
+    //         .wrap_unsafe_ops(true)
+    //         .generate_comments(true)
+    //         .layout_tests(false)
+    //         .generate()
+    //         .expect("Unable to generate bindings");
 
-//     let out_path = std::env::current_dir().unwrap();
+    //     let out_path = std::env::current_dir().unwrap();
 
-//     bindings
-//         .write_to_file(out_path.join("src/libopenbabel.rs"))
-//         .expect("Couldn't write bindings!");
-
+    //     bindings
+    //         .write_to_file(out_path.join("src/libopenbabel.rs"))
+    //         .expect("Couldn't write bindings!");
 }
